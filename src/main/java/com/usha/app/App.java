d57eccc6b1ebf664e63a8fba2c86a3b7a4a95330
Ubuntu <ubuntu@ip-172-31-20-213.eu-north-1.compute.internal>
2026-02-16 17:03:41 +0000

    Initial commit - AWS Java App

---
+++ src/main/java/com/usha/app/App.java
@@ -0,0 +1,13 @@
+package com.usha.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
